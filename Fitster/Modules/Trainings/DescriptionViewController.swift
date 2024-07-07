49fea9a29aef4280a4009eac505b2ab752611b43
Dmitry Leontev <dmitrii.leontev10200@mail.ru>
2024-07-07 18:53:32 +0500

    make trainings

---
+++ Fitster/Modules/Trainings/DescriptionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DescriptionViewController.swift
+//  Fitster
+//
+//  Created by Дмитрий Леонтьев on 06.07.2024.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+    var exercise: Exercise?
+    
+    @IBOutlet weak var titleExercise: UILabel!
+    @IBOutlet weak var imageExercise: UIImageView!
+    @IBOutlet weak var descriptionExercise: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.titleExercise.text = exercise?.title
+        self.imageExercise.image = UIImage(named: exercise!.image)
+        self.descriptionExercise.text = exercise?.description
+    }
+}
