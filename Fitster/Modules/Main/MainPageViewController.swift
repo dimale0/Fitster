d26e4b61ac449151d14cbfd58a8b7bac343ee3a9
Artem Tabachenko <fexo5467@gmail.com>
2024-07-08 17:28:39 +0300

    Тимур газуй

--- Fitster/Modules/Main/MainPageViewController.swift
+++ Fitster/Modules/Main/MainPageViewController.swift
@@ -10,68 +10,86 @@ import UIKit
 class MainPageViewController: UIViewController {
     let defaults = UserDefaults.standard
     
-    
     var dayCalories = 0.0
-   
-
+    
     @IBOutlet weak var heightLabel: UILabel!
-    
     @IBOutlet weak var weightLabel: UILabel!
-    
     @IBOutlet weak var optionLabel: UILabel!
     
+    enum Option: String {
+        case massGain = "Набор массы"
+        case weightMaintenance = "Поддержание веса"
+        case weightLoss = "Уменьшение веса"
+    }
+    
+    struct UserDefaultsKeys {
+        static let savedHeight = "savedHeight"
+        static let savedWeight = "savedWeight"
+        static let savedAge = "savedAge"
+        static let selectedOption = "selectedOption"
+        static let savedSex = "savedSex"
+        static let isAllDataSaved = "isAllDataSaved"
+    }
+    
     override func viewDidLoad() {
-//        UserDefaults.standard.removeObject(forKey: "savedHeight")
-//        UserDefaults.standard.removeObject(forKey: "savedWeight")
-//        UserDefaults.standard.removeObject(forKey: "savedAge")
-//        UserDefaults.standard.removeObject(forKey: "selectedOption")
-//        UserDefaults.standard.removeObject(forKey: "savedSex")
-//        UserDefaults.standard.removeObject(forKey: "isAllDataSaved")
-
         super.viewDidLoad()
         
-        if (defaults.value(forKey: "isAllDataSaved") == nil){
-            print(defaults.value(forKey: "isAllDataSaved") as Any)
+        if defaults.value(forKey: UserDefaultsKeys.isAllDataSaved) == nil {
+            print(defaults.value(forKey: UserDefaultsKeys.isAllDataSaved) as Any)
             performSegue(withIdentifier: "goToInputParameters", sender: nil)
-        }
-
-        self.heightLabel.text = defaults.string(forKey: "savedHeight")
-        self.weightLabel.text = defaults.string(forKey: "savedWeight")
-        self.optionLabel.text = defaults.string(forKey: "selectedOption")
-        
-        if ((defaults.value(forKey: "isAllDataSaved") != nil) == true){
-            var option = defaults.string(forKey: "selectedOption")
-            var height = defaults.string(forKey: "savedHeight")
-            var weight = defaults.string(forKey: "savedWeight")
-            var age = defaults.string(forKey: "savedAge")
-            var sex = defaults.string(forKey: "savedSex")
-            
-            switch option{
-            case "Набор массы":
-                if sex as! String == "male"{
-                    dayCalories = (66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age)) * 1.6
-                }else{
-                    dayCalories = (655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age)) * 1.6
-                }
-            case "Поддержание веса":
-                if sex as! String == "male"{
-                    dayCalories = (66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age)) * 1.3
-                }else{
-                    dayCalories = (655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age)) * 1.3
-                }
-            case "Уменьшение веса":
-                if sex as! String == "male"{
-                    dayCalories = (66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age)) * 0.85
-                }else{
-                    dayCalories = (655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age)) * 0.85
-                }
-            default:
-                dayCalories = 0
-            }
+            return
         }
         
+        heightLabel.text = defaults.string(forKey: UserDefaultsKeys.savedHeight)
+        weightLabel.text = defaults.string(forKey: UserDefaultsKeys.savedWeight)
+        optionLabel.text = defaults.string(forKey: UserDefaultsKeys.selectedOption)
+        
+        guard let optionString = defaults.string(forKey: UserDefaultsKeys.selectedOption),
+              let option = Option(rawValue: optionString),
+              let heightString = defaults.string(forKey: UserDefaultsKeys.savedHeight),
+              let weightString = defaults.string(forKey: UserDefaultsKeys.savedWeight),
+              let ageString = defaults.string(forKey: UserDefaultsKeys.savedAge),
+              let sex = defaults.string(forKey: UserDefaultsKeys.savedSex),
+              let height = Double(heightString),
+              let weight = Double(weightString),
+              let age = Double(ageString) 
+        else {
+            return
+        }
+        
+        dayCalories = calculateCalories(
+            option: option, 
+            height: height,
+            weight: weight,
+            age: age,
+            sex: sex
+        )
     }
-
-
+    
+    private func calculateCalories(
+        option: Option,
+        height: Double,
+        weight: Double,
+        age: Double,
+        sex: String
+    ) -> Double {
+        let isMale = sex == "male"
+        var bmr: Double
+        
+        if isMale {
+            bmr = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age)
+        } else {
+            bmr = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age)
+        }
+        
+        switch option {
+        case .massGain:
+            return bmr * 1.6
+        case .weightMaintenance:
+            return bmr * 1.3
+        case .weightLoss:
+            return bmr * 0.85
+        }
+    }
 }
 
